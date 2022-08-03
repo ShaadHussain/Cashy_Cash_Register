43b9bacf0ba795c62038f79f15e489f91a465991
shaadhussain <hussai19@purdue.edu>
2022-08-02 19:29:33 -0500

    Figured out data persistence with csv

--- benetech_iv.py
+++ benetech_iv.py
@@ -13,92 +13,80 @@ def init_curr_map():
     currency_map[10.0] = 100
     currency_map[20.0] = 150
 
+
+
+# Taking the user's payment (deposit) and allocating it to register
+def replenishRegister(deposit):
+    amounts = [20.0, 10.0, 5.0, 1.0, 0.5, 0.25, 0.1, 0.05, 0.01]
+
+    deposit = round(deposit, 2)
+
+    for amount in amounts:
+
+            while deposit >= amount:
+
+                print(f"Curr amount: {amount}")
+                print(f"Curr deposit before: {deposit}")
+
+                currency_map[amount] += 1
+
+                deposit -= amount
+
+                deposit = round(deposit, 2)
+
+                # replenishing the register by adding to it
+
+
+
 def makeChange(price, payment):
 
-    curr_arr = [20.0, 10.0, 5.0, 1.0, 0.5, 0.25, 0.1, 0.05, 0.01]
+    amounts = [20.0, 10.0, 5.0, 1.0, 0.5, 0.25, 0.1, 0.05, 0.01]
 
     user_change_map = {}
     change = (payment - price)
+
+    print(f"Change b4 loop: {change}")
+
+    change = round(change, 2)
+
     if change > 0:
-        
-        while change >= 20.0 and currency_map[20.0] != 0:
 
+        replenishRegister(price)
 
-            change -= 20.0
-            currency_map[20.0] -= 1
+        for amount in amounts:
 
-            user_change_map[20.0] = user_change_map.get(20.0, 0) + 1
+            while change >= amount and currency_map[amount] != 0:
 
-        
-        while change >= 10.0 and currency_map[10.0] != 0:
+                print(f"Curr amount: {amount}")
+                print(f"Curr change before: {change}")
 
-            change -= 10.0
-            currency_map[10.0] -= 1
+                change -= amount
 
-            user_change_map[10.0] = user_change_map.get(10.0, 0) + 1
+                change = round(change, 2)
+                currency_map[amount] -= 1
 
+                user_change_map[amount] = user_change_map.get(amount, 0) + 1
+            
 
-        while change >= 5.0 and currency_map[5.0] != 0:
-
-            change -= 5.0
-            currency_map[5.0] -= 1
-
-            user_change_map[5.0] = user_change_map.get(5.0, 0) + 1
-
-        
-        while change >= 1.0 and currency_map[1.0] != 0:
-
-            change -= 1.0
-            currency_map[1.0] -= 1
-
-            user_change_map[1.0] = user_change_map.get(1.0, 0) + 1
-
-        
-        while change >= 0.5 and currency_map[0.5] != 0:
-
-            change -= 0.5
-            currency_map[0.5] -= 1
-
-            user_change_map[0.5] = user_change_map.get(0.5, 0) + 1
-
-        
-        while change >= 0.25 and currency_map[0.25] != 0:
-
-            change -= 0.25
-            currency_map[0.25] -= 1
-
-            user_change_map[0.25] = user_change_map.get(0.25, 0) + 1
-
-
-        while change >= 0.1 and currency_map[0.1] != 0:
-
-            change -= 0.1
-            currency_map[0.1] -= 1
-
-            user_change_map[0.1] = user_change_map.get(0.1, 0) + 1
-
-
-        while change >= 0.05 and currency_map[0.05] != 0:
-
-            change -= 0.05
-            currency_map[0.05] -= 1
-
-            user_change_map[0.05] = user_change_map.get(0.05, 0) + 1
-
-        
-        while change >= 0.01 and currency_map[0.01] != 0:
-
-            change -= 0.01
-            currency_map[0.01] -= 1
-
-            user_change_map[0.01] = user_change_map.get(0.01, 0) + 1
-
-        
         print(f"-- Final change -- ")
+
+        for amount in amounts:
+            if amount >= 1.0:
+                print(f"Num of ${amount}: {user_change_map.get(amount, 0)}")
+            elif 0 <= amount < 1.0:
+                print(f"Num of {amount}¢: {user_change_map.get(amount, 0)}")
+
         
     elif change < 0:
         print("Error! Payment is not sufficient.")
 
+    elif change == 0:
+        print("No change")
+
+
+init_curr_map()
+makeChange(400.33, 400.33)
+print(str(currency_map))
     
 
         
--- test.py
+++ test.py
@@ -1,7 +1,53 @@
-hmap = {}
+# hmap = {}
 
-hmap[20.0] = 5
+# hmap[20.0] = 5
 
-x = hmap.get(20, 0)
+# x = hmap.get(20, 0)
 
-print(f"X: {x}")
+
+# Testing rounding
+# num = 29.399999
+
+# num = round(num, 2)
+# print(f"Num: {num}")
+
+# Testing dictReader
+
+import csv
+
+with open('register.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    line_ct = 0
+
+    amt_num_map = {}
+
+    amounts = []
+    amounts_nums = []
+    for row in csv_reader:
+        if line_ct == 0:
+            line_ct += 1
+
+            # amounts = ",".join(row)
+            amounts = list(row)
+
+            amounts = [int(num) for num in amounts]
+
+            print(f"Row 0: {amounts}")
+
+
+        elif line_ct == 1:
+            # amounts_nums = ",".join(row)
+            amounts_nums = list(row)
+
+            amounts_nums = [int(num) for num in amounts_nums]
+
+            print(f"Row 1: {amounts_nums}")
+            # line_ct += 1
+
+
+    for i in range(len(amounts)):
+        amt_num_map[amounts[i]] = amounts_nums[i]
+
+
+    print(str(dict(amt_num_map)))
